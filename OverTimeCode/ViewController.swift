f4231c5d8e73638aa6571279f0fdb9a31a4884e2
woyoushmy8 <woyoushmy8@126.com>
2017-01-21 11:58:15 +0800

    first commit

---
+++ OverTimeCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OverTimeCode
+//
+//  Created by mike bian on 2017/1/21.
+//  Copyright © 2017年 mike bian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
